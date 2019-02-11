5e958b6a4d48c843c185f578961020dbc8bb6bd6
tuvokki <tuvokki@gmail.com>
2019-02-11 10:53:30 +0100

    production changes

--- dividi/settings.py
+++ dividi/settings.py
@@ -19,13 +19,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*d4%8(a^qr)59eaquu#u-^=1r2yh!)k8f=ey^c*%g(^pt5jria'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    '149.210.160.81',
+    'tvk.tuvok.nl',
+]
 
 
 # Application definition
@@ -57,8 +60,7 @@ ROOT_URLCONF = 'dividi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -87,13 +89,27 @@ WSGI_APPLICATION = 'dividi.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if not DEBUG:
+    # SECURITY WARNING: keep the secret key used in production secret!
+    with open('/home/tuvok/domains/tvk.tuvok.nl/etc/databa_key.txt') as f:
+        DATABA_KEY = f.read().strip()
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'dividi',
+            'USER': 'dividi',
+            'PASSWORD': DATABA_KEY,
+            'HOST': 'localhost',
+            'PORT': '',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
@@ -137,3 +153,19 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+# Production settings
+if not DEBUG:
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    X_FRAME_OPTIONS = 'DENY'
+    # SECURITY WARNING: keep the secret key used in production secret!
+    with open('/home/tuvok/domains/tvk.tuvok.nl/etc/secret_key.txt') as f:
+        SECRET_KEY = f.read().strip()
+    STATIC_ROOT = '/home/tuvok/domains/tvk.tuvok.nl/public_html/static/'
+else:
+    SECRET_KEY = '*d4%8(a^qr)59eaquu#u-^=1r2yh!)k8f=ey^c*%g(^pt5jria'
+
