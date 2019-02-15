fe8ee6c36a6ff1546eb43e4886ec1a0846d2292b
Wouter <tuvokki@gmail.com>
2019-02-15 11:50:36 +0100

    Generate Fib model and Authenticated API

--- tholisa/serializers.py
+++ tholisa/serializers.py
@@ -2,7 +2,7 @@ from django.utils.text import slugify
 from rest_framework import serializers
 
 from accounts.models import Ibutho
-from .models import Pensis
+from .models import Pensis, Fib
 
 
 class PensisSerializer(serializers.Serializer):
@@ -29,3 +29,30 @@ class PensisSerializer(serializers.Serializer):
         instance.user = posted_user
         instance.save()
         return instance
+
+
+class FibSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Fib
+        fields = ('id', 'title', 'content', 'slug', 'user')
+
+# class FibSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(required=True, allow_blank=False, max_length=80)
+#     content = serializers.CharField(required=True)
+#     slug = serializers.SlugField(required=False)
+#     user = serializers.CharField(required=True)
+#
+#     def create(self, validated_data):
+#         posted_user = Ibutho.objects.filter(id=validated_data['user']).first()
+#         validated_data['user'] = posted_user
+#         validated_data['slug'] = slugify(validated_data['title'])
+#         return Fib.objects.create(**validated_data)
+#
+#     def update(self, instance, validated_data):
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.content = validated_data.get('content', instance.content)
+#         posted_user = Ibutho.objects.filter(id=validated_data.get('user', instance.user.id)).first()
+#         instance.user = posted_user
+#         instance.save()
+#         return instance
